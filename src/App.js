82b24e2cf819b336b45cfeef71e38014fdc4acd4
Oluwaseun-Gbenle <62512861+Oluwaseun-Gbenle@users.noreply.github.com>
2020-08-25 19:53:45 +0100

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { Component } from "react";  
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+       this.state = {
+      products: {}
+    } 
+     
+      }
+   
+    componentDidMount() {
+      axios.get('https://striped-stripe-ulna.glitch.me/api/reorder')
+      .then(res => {
+        this.setState({ products: res.data })
+        console.log(this.state.products)
+      })
+    }
+
+     render(){
+    return(
+    <div className='container'>
+    <div className="row">
+    {
+    this.state.products.map((product)=>( 
+    <div>
+    <div className='col-xs-12'>
+     <p>{product.inventory.name}</p>  
+   </div>
+   <div class="col-xs-12">
+       <p>FOOD AND BEVERAGE</p>
+   </div>   
+   <div class="col-xs-12">
+   <p>{product.inventory.barcode}</p>
+   </div> 
+   <div class="col-xs-12">
+      <button>edit</button>   
+   </div> 
+   <div class="col-xs-12">
+      <p>cost</p> 
+      <p>{product.inventory.unit_cost_price}</p>
+      <p>selling</p>
+      <p>{product.inventory.unit_selling_price}</p>  
+   </div> 
+   <div class="col-xs-12">
+     {product.inventory.expiry_date}     
+   </div> 
+   <div class="col-xs-12">
+      <p>{product.inventory.created_at}</p>    
+   </div> 
+   </div>
+ ))
+ }                                                                                                                                                                                                                                                                                                                                                                                            
+   </div>
+   </div>
+      )   
 }
+  }
 
 export default App;
