bd45ddb81acfef89deada4300800750f67c3002e
Oluwaseun-Gbenle <62512861+Oluwaseun-Gbenle@users.noreply.github.com>
2020-08-27 00:30:28 +0100

    Updates

---
+++ precache-manifest.5c9d209c68a3706006b11e8c06bcc319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40bc0ee8acbdd824e7fae2776efd4e5",
+    "url": "/productlist/index.html"
+  },
+  {
+    "revision": "362c8301af026cf56efa",
+    "url": "/productlist/static/css/main.fbffb6ea.chunk.css"
+  },
+  {
+    "revision": "784f914e4582ef9639cf",
+    "url": "/productlist/static/js/2.28e6852b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/productlist/static/js/2.28e6852b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362c8301af026cf56efa",
+    "url": "/productlist/static/js/main.c3a75084.chunk.js"
+  },
+  {
+    "revision": "1bc61ac679166d3c2828",
+    "url": "/productlist/static/js/runtime-main.b06fb330.js"
+  }
+]);
